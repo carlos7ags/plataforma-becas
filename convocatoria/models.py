2eb49d40de739b9b63fda74e84f2f64c07abdafe
Carlos Munoz <carlos7ags@gmail.com>
2020-12-27 20:51:55 -0600

    feat(convocatoria): create aspirants model
    Signed-off-by: Carlos Munoz <carlos.munoz@cornershop.mx>

--- convocatoria/models.py
+++ convocatoria/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.conf import settings
 
 
 class Modalidades(models.Model):
@@ -12,8 +13,8 @@ class Modalidades(models.Model):
 class Convocatorias(models.Model):
     """Información convocatorias"""
 
-    convocatoria_id = models.AutoField(primary_key=True
-    )
+    convocatoria_id = models.AutoField(primary_key=True)
+    codigo = models.CharField("Código", max_length=8)
     name = models.CharField("Nombre", max_length=30)
     description = models.CharField("Descripción", max_length=256)
     start_date = models.DateField("Fecha de apertura")
@@ -24,7 +25,31 @@ class Convocatorias(models.Model):
         on_delete=models.CASCADE,
         verbose_name="Modalidad",
     )
-    #presupuesto = models.IntegerField("Presupuesto asignado")
+    presupuesto = models.IntegerField("Presupuesto asignado")
 
     def __str__(self):
         return "%s" % self.name
+
+
+class Aspirantes(models.Model):
+    """Información convocatorias"""
+
+    folio = models.AutoField(primary_key=True)
+    convocatoria = models.ForeignKey(
+        to=Convocatorias,
+        on_delete=models.CASCADE,
+        verbose_name="Convocatoria",
+    )
+    username = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        editable=False,
+    )
+    grade = models.IntegerField("Calificación", null=True)
+    socioeconomic_score = models.IntegerField("Estudio socioeconómico", null=True)
+    grneral_score = models.IntegerField("Estudio socioeconómico", null=True)
+    beneficiado = models.BooleanField("Beneficiado", null=True)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return "%s" % self.folio
--- convocatoria/views.py
+++ convocatoria/views.py
@@ -10,6 +10,9 @@ class ConvocatoriasView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         convocatorias = Convocatorias.objects.all().order_by("-end_date")
-        status = [convoc.start_date <= date.today() <= convoc.end_date for convoc in convocatorias]
-        context['convocatorias'] = zip(convocatorias, status)
+        status = [
+            convoc.start_date <= date.today() <= convoc.end_date
+            for convoc in convocatorias
+        ]
+        context["convocatorias"] = zip(convocatorias, status)
         return context
